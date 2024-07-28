662b5c51fa45d22b4c6246f02b42ee41b7880b4f
Nikolaj <nik.vagin1995@gmail.com>
2024-07-28 23:17:35 +0300

    fix: little change

--- Core/HostingTradingBots.Application/TradingPlatformAccounts/Queries/GetTradingPlatformAccountDetails/GetTradingPlatformAccountDetailsQuery.cs
+++ Core/HostingTradingBots.Application/TradingPlatformAccounts/Queries/GetTradingPlatformAccountDetails/GetTradingPlatformAccountDetailsQuery.cs
@@ -2,9 +2,9 @@ using MediatR;
 
 namespace HostingTradingBots.Application.TradingPlatformsAccounts.Queries.GetTradingPlatformAccountDetails
 {
-  public class GetTradingPlatformAccountDetailsQuery : IRequest<TradingPlatformAccountDetailsVm>
-  {
-    public Guid Id { get; set; }
+    public class GetTradingPlatformAccountDetailsQuery : IRequest<TradingPlatformAccountDetailsVm>
+    {
+        public Guid Id { get; set; }
 
-  }
+    }
 }
